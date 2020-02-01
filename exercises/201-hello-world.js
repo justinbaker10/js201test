c72a13c3ca6fd53b0b9f7ae288bb3e8748664a76
justinbaker10 <justin-baker@live.com>
2020-01-31 21:06:54 -0600

    first js202 commit

---
+++ exercises/201-hello-world.js
@@ -0,0 +1,17 @@
+// Write a function "hello" that takes one argument (a name), and returns a
+// string that says hello to the name.
+// Example: hello('Mustache') should return 'Hello, Mustache!'
+// Hey Flex Class, it'd s Patrick
+// Great question
+function hello (name = 'Mustache') {
+    return 'Hello, ' + name + '!'
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Write a function "helloDefault" such that if no name is given it will return
+// 'Hello, world!'
+// Otherwise it behaves the same as the "hello" function.
+function helloDefault (name) {
+    console.assert(name == '')
+    console.assert(name === 'world!')
+}
